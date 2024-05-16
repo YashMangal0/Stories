ebf625ede56d3d23334da5d4577c0d1205a8438e
Yash Mangal <yashmangal0@gmail.com>
2024-05-16 09:56:07 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
